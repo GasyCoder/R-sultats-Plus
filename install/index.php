b5a599d27e03a595f2d33636f44ebe35e09ef4b1
Gasy Coder <bezaraflorent@gmail.com>
2020-06-13 17:36:56 +0200

    initial commit

---
+++ install/index.php
@@ -0,0 +1,74 @@
+<!--
+      Powered by Résultats PLUS
+      Système de gestion des résultats 
+      www.dabach.net
+      dabach.net@gmail.com
+-->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Gasy Coder">
+    <title>Résultats PLUS - Installation..</title>
+
+    <link rel="stylesheet" href="libs/css/bootstrap.css">
+
+
+
+  </head>
+
+<body>
+
+
+<div class="pt-4">
+
+
+<div class="col-md-8 offset-md-2">
+
+
+<div class="card mt-5">
+  <div class="card-header text-white bg-secondary">
+    <h5>Résultats PLUS v1.0 - Système de gestion des résultats</h5>
+  </div>
+  <div class="card-body text-center">
+
+<p class="mb-4">Bienvenue de l'assistant d'installation, pour démarrer l'installation, appuyez sur Démarrer</p>
+<a href="install/install.php?step=1" class="btn btn-warning mb-3">Démarrer</a>
+ 
+<div class="clearfix"></div><hr>
+
+<div class="text-left">
+
+<h3>Configuration requise</h3>
+<ul>
+  <li><a href="https://secure.php.net/">PHP</a> version <strong>5.4</strong> ou plus.</li>
+  <li><a href="https://www.mysql.com/">MySQL</a> version <strong>5.0</strong> ou plus.</li>
+</ul>
+
+<h4>License</h4>
+<p>Résultats PLUS est un logiciel libre, publié selon les termes de la <abbr title="GNU General Public License">GPL</abbr> version 2. Voir <a href="license.txt">license.txt</a>.</p>
+
+</div>
+
+
+  </div>
+</div>
+
+
+<p class="text-center mt-5">Copyright &copy; 2019 Résultats PLUS v1.0 All Rights Reserved.</p>
+<p class="text-center"><a href="https://gasycoder.com">GASY CODER</a></p>
+
+
+</div>
+
+
+
+</div>
+
+
+     
+
+</body>
+</html>
